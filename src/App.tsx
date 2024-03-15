6cdb91616b61e16c7f807b0d2d83f022228d69d8
Nicostudent <rojo.nico@hotmail.com>
2024-03-15 09:51:24 -0300

    second

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <div className="flex flex-col justify-between items-center  h-screen">
-      <h1 className='m-2 pt-10  h-1/3 font-mono text-3xl font-bold  text-yellow-300 hover:animate-pulse		'>unamultimedia.com</h1>
-      <div className='  p-2 h-1/3'>
+      <h1 className='mt-10 font-mono text-3xl font-bold  text-yellow-300 animate-pulse	hover:animate-none hover:text-yellow-100 hover:cursor-pointer	'>unamultimedia.com</h1>
+      <div className='  p-2 h-2/3  flex justify-center align-middle items-center'>
         <audio controls>
           <source src={audioSource} type="audio/mpeg" />
           Your browser does not support the audio element.
